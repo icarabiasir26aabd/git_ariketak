ec58eb2d37935b8675525fe489da1c96530d822f
icarabiasir26aabd <icarabiasir26aab@ikzubirimanteo.com>
2026-09-18 08:35:05 +0200

    GitHub aldaketa

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 print("The missile knows where it is at all times. It knows this because it knows where it isn't. By subtracting where it is from where it isn't.")
 
 print("Aldaketa egin.")
+
+print("GitHub aldaketa.")
